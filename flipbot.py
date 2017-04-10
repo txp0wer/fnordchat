8e8cfa10a610a61bb8c4a8b549a11796d7eb3758
txp0wer <txp0wer@noreply.users.github.com>
2017-04-10 19:03:18 +0200

    added bot to simulate muccc's flipdot

---
+++ flipbot.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python2
+# Author: txp0wer (github.com/txp0wer)
+# License: GNU GPLv3
+
+import sys,os,fnordchat;
+
+prefix=os.getenv("ADDR_PREFIX") or "ff08"
+
+c=fnordchat.chat(["darkfasel#ccc"],prefix="ff02",nick="flipbot")
+def talk_handler(timestamp,nick,addr,method,text):
+    if method=="normal" and "alarm" in text:
+        message=nick+": "+text
+        c.talk("flipped to: "+message,method="me")
+        # do something with text
+c.add_handler("talk",talk_handler)
